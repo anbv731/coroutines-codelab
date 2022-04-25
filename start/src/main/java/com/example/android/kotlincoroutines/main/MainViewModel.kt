8b53039e8f1bdedee8f3250268d7f278871b629b
anbv731 <anbv731@yandex.ru>
2022-04-25 14:42:57 +0400

    task 14 coroutines and flow finish

--- start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -19,8 +19,12 @@ package com.example.android.kotlincoroutines.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.android.kotlincoroutines.util.BACKGROUND
 import com.example.android.kotlincoroutines.util.singleArgViewModelFactory
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * MainViewModel designed to store and manage UI-related data in a lifecycle conscious way. This
@@ -31,6 +35,18 @@ import com.example.android.kotlincoroutines.util.singleArgViewModelFactory
  * @param repository the data source this ViewModel will fetch results from.
  */
 class MainViewModel(private val repository: TitleRepository) : ViewModel() {
+    private fun launchDataLoad(block: suspend () -> Unit): Job {
+        return viewModelScope.launch {
+            try {
+                _spinner.value = true
+                block()
+            } catch (error: TitleRefreshError) {
+                _snackBar.value = error.message
+            } finally {
+                _spinner.value = false
+            }
+        }
+    }
 
     companion object {
         /**
@@ -101,11 +117,14 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
      * Wait one second then update the tap count.
      */
     private fun updateTaps() {
-        // TODO: Convert updateTaps to use coroutines
-        tapCount++
-        BACKGROUND.submit {
-            Thread.sleep(1_000)
+        viewModelScope.launch {
+            tapCount++
+            delay(1000)
             _taps.postValue("${tapCount} taps")
+//            BACKGROUND.submit {
+//                //Thread.sleep(1_000)
+//                _taps.postValue("${tapCount} taps")
+//            }
         }
     }
 
@@ -119,18 +138,23 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
     /**
      * Refresh the title, showing a loading spinner while it refreshes and errors via snackbar.
      */
+//    fun refreshTitle() {
+//        // TODO: Convert refreshTitle to use coroutines
+//        _spinner.value = true
+//        repository.refreshTitleWithCallbacks(object : TitleRefreshCallback {
+//            override fun onCompleted() {
+//                _spinner.postValue(false)
+//            }
+//
+//            override fun onError(cause: Throwable) {
+//                _snackBar.postValue(cause.message)
+//                _spinner.postValue(false)
+//            }
+//        })
+//    }
     fun refreshTitle() {
-        // TODO: Convert refreshTitle to use coroutines
-        _spinner.value = true
-        repository.refreshTitleWithCallbacks(object : TitleRefreshCallback {
-            override fun onCompleted() {
-                _spinner.postValue(false)
-            }
-
-            override fun onError(cause: Throwable) {
-                _snackBar.postValue(cause.message)
-                _spinner.postValue(false)
-            }
-        })
+        launchDataLoad {
+            repository.refreshTitle()
+        }
     }
 }
